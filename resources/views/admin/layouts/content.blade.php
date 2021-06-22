97f4738a2cd0c813f6537128f715292562cca66a
Tạ Trần An <anttph12358@gmail.com>
2021-06-22 17:15:56 +0700

    Chia Layouts

---
+++ resources/views/admin/layouts/content.blade.php
@@ -0,0 +1,36 @@
+<div class="page-wrapper" style="min-height: 250px;">
+
+    <div class="page-breadcrumb bg-white">
+        <div class="row align-items-center">
+            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                <h4 class="page-title">Blank Page</h4>
+            </div>
+            <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                <div class="d-md-flex">
+                    <ol class="breadcrumb ms-auto">
+                        <li><a href="#" class="fw-normal">Dashboard</a></li>
+                    </ol>
+                    <a href="https://www.wrappixel.com/templates/ampleadmin/" target="_blank" class="btn btn-danger  d-none d-md-block pull-right ms-3 hidden-xs hidden-sm waves-effect waves-light text-white">Upgrade
+                        to Pro</a>
+                </div>
+            </div>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+
+    <div class="container-fluid">
+        @yield('content')
+        <!-- <div class="row">
+            <div class="col-md-12">
+                <div class="white-box">
+                    <h3 class="box-title">Blank Page</h3>
+                </div>
+            </div>
+        </div> -->
+
+    </div>
+
+    <footer class="footer text-center"> 2021 © Ample Admin brought to you by <a href="https://www.wrappixel.com/">wrappixel.com</a>
+    </footer>
+
+</div>
