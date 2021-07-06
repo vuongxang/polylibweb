0cfa3a750552b9f6b9ccdaa1f60ce9f9a910ad6c
Tạ Trần An <anttph12358@gmail.com>
2021-07-07 01:33:36 +0700

    Edit Icon Admin

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <link rel="icon" href="{{asset('images/logo.png')}}">
     <title> @yield('title','Polylib admin') </title>
     <!-- Custom fonts for this template-->
     @include('admin.layouts.style')
