a551fba1dd740e7d37da4ea8b0d59335f3e092c6
Tạ Trần An <anttph12358@gmail.com>
2021-07-07 03:14:07 +0700

    Edit Banner

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="{{asset('images/logo.png')}}">
-    <title> @yield('title','Polylib admin') </title>
+    <title> @yield('title','Admin') </title>
     <!-- Custom fonts for this template-->
     @include('admin.layouts.style')
 
@@ -63,17 +63,17 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Đăng xuất</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Hủy</button>
                     <form action="{{route('logout')}}" method="post">
                         @csrf
-                        <button class="btn btn-primary" type="submit">Logout</button>
+                        <button class="btn btn-primary" type="submit">Đăng xuất</button>
                     </form>
                 </div>
             </div>
--- resources/views/client/pages/home.blade.php
+++ resources/views/client/pages/home.blade.php
@@ -6,26 +6,53 @@
 @section('content')
 
 <main>
-    <div class="banner">
-        <div class="banner-content">
-            <div class="banner-content__heading">
-                <h2><span>Sách</span></h2>
-                <h2> Bạn muốn đọc là gì?</h2>
+    <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner ">
+            <div class="carousel-item active ">
+                <div class="banner banner-first">
+                    <div class="banner-content">
+                        <div class="banner-content__heading">
+                            <h2>POLYLIB</h2>
+                            <p class="banner-content__quote">Một cuốn sách thực sự hay nên đọc trong tuổi trẻ, rồi đọc lại khi đã trưởng thành, và một nữa lúc tuổi già, giống như một tòa nhà đẹp nên được chiêm ngưỡng trong ánh bình minh, nắng trưa và ánh trăng.</p>
+                        </div>
+                        <button class="button btn--view-all">Đọc ngay</button>
+                    </div>
+                    <div class="banner-image">
+                        <img src="{{asset('images/Bookmarks2.gif')}}" alt="">
+                    </div>
+                </div>
             </div>
-            <p class="banner-content__body">Tìm kiếm ngay thôi nào</p>
-            <div class="banner-content__search">
-                <form action="" class="search-form">
-                    <input class="search-input" type="text" placeholder="Tìm kiếm theo tên sách, danh mục, tác giả">
-                    <button class="search-btn">
-                        <i class="fas fa-search"></i>
-                    </button>
-                </form>
+            <div class="carousel-item">
+                <div class="banner banner-second">
+                    <div class="banner-content">
+                        <div class="banner-content__heading banner-content__heading--second">
+                            <h2>POLYLIB</h2>
+                            <p class="banner-content__quote banner-content__quote--second ">Đằng sau sự thành công của một người đàn ông, là hình dáng của một người phụ nữ. Còn đằng sau sự thành công của bất kì ai là ít nhất một cuốn sách, hay cả một giá sách.</p>
+                        </div>
+                        <button class="button btn--view-all btn--view-all--second">Đọc ngay</button>
+                    </div>
+                    <div class="banner-image">
+                        <img src="{{asset('images/Bookmarks.gif')}}" alt="">
+                    </div>
+                </div>
             </div>
+            
         </div>
-        <div class="banner-image">
-            <img src="{{asset('images/banner.svg')}}" alt="">
-        </div>
+        <a class="carousel-control-prev px-4" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon ml-4" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next " href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon mr-4" aria-hidden="true"></span>
+            <span class="sr-only ">Next</span>
+        </a>
     </div>
+
 
 
     <div class="book-list book__list--nobackground">
@@ -33,56 +60,54 @@
             <h2>Sách mới nhất</h2>
 
         </div>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-pause ="hover">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-pause="hover">
             <div class="carousel-inner ">
                 <div class="carousel-item active">
                     <div class="row  justify-content-center">
                         @foreach ($books as $book)
-                            @if($loop->index < 4)
-                                <div class="col-3  ">
-                                    <div class="book-item">
-                                        <img src="{{asset($book->image)}}" alt="">
-                                        <h3>{{$book->title}}</h3>
-                                        @foreach($book->authors as $bookAuthor)
-                                        <p> <span class="book-author"> {{$bookAuthor->name}}</span></p>
+                        @if($loop->index < 4) <div class="col-3  ">
+                            <div class="book-item">
+                                <img src="{{asset($book->image)}}" alt="">
+                                <h3>{{$book->title}}</h3>
+                                @foreach($book->authors as $bookAuthor)
+                                <p> <span class="book-author"> {{$bookAuthor->name}}</span></p>
 
-                                        @endforeach
-                                        <p> <span class="book-star"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></span></p>
-                                    </div>
-                                </div>
-                            @endif
-                        @endforeach
-                    </div>
-                </div>
-                <div class="carousel-item ">
-                    <div class="row  justify-content-center">
-                        @foreach ($books as $book)
-
-                        @if($loop->index >= 4 && $loop->index <= 8)
-                            <div class="col-3  ">
-                                <div class="book-item">
-                                    <img src="{{asset($book->image)}}" alt="">
-                                    <h3>{{$book->title}}</h3>
-                                    @foreach($book->authors as $bookAuthor)
-                                    <p> <span class="book-author"> {{$bookAuthor->name}}</span></p>
-
-                                    @endforeach
-                                    <p> <span class="book-star"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></span></p>
-                                </div>
+                                @endforeach
+                                <p> <span class="book-star"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></span></p>
                             </div>
-                        @endif
-                        @endforeach
                     </div>
+                    @endif
+                    @endforeach
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <button class="carousel-btn-custom"><i class="fas fa-chevron-left"></i></button>
-                <!-- <span class="sr-only">Previous</span> -->
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <button class="carousel-btn-custom"><i class="fas fa-chevron-right"></i></button>
-            </a>
+            <div class="carousel-item ">
+                <div class="row  justify-content-center">
+                    @foreach ($books as $book)
+
+                    @if($loop->index >= 4 && $loop->index <= 8) <div class="col-3  ">
+                        <div class="book-item">
+                            <img src="{{asset($book->image)}}" alt="">
+                            <h3>{{$book->title}}</h3>
+                            @foreach($book->authors as $bookAuthor)
+                            <p> <span class="book-author"> {{$bookAuthor->name}}</span></p>
+
+                            @endforeach
+                            <p> <span class="book-star"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></span></p>
+                        </div>
+                </div>
+                @endif
+                @endforeach
+            </div>
         </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <button class="carousel-btn-custom"><i class="fas fa-chevron-left"></i></button>
+        <!-- <span class="sr-only">Previous</span> -->
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <button class="carousel-btn-custom"><i class="fas fa-chevron-right"></i></button>
+    </a>
+    </div>
 
 
 
