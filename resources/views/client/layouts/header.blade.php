16ac87b8a5136d2c29b2014df2cd6b7d2b336093
Tạ Trần An <anttph12358@gmail.com>
2021-07-19 17:22:23 +0700

    Edit Dropdown Header

--- resources/views/client/layouts/header.blade.php
+++ resources/views/client/layouts/header.blade.php
@@ -25,34 +25,35 @@
         <div class="header__information-notification">
             <img src="{{asset('images/notification.svg')}}" alt="">
         </div>
-        <a id="navbarDropdown" class="nav-link  header__information-info" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            <img src="{{Auth::user()->avatar}}" alt="">
-            <i class="fas fa-caret-down"></i>
-        </a>
-        @if (Auth::check())
+        <div>
+            <a id="navbarDropdown" class="nav-link  header__information-info" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                <img src="{{Auth::user()->avatar}}" alt="">
+                <i class="fas fa-caret-down"></i>
+            </a>
+            @if (Auth::check())
             <div class="dropdown-menu dropdown-menu-right dropdown-custom" aria-labelledby="navbarDropdown">
                 @if (Auth::user()->role_id == 1 || Auth::user()->role_id == 2)
                 <a class="dropdown-item dropdown-item-custom" href="{{ route('dashboard') }}">
-                    <img src="{{asset('images/admin-icon.svg')}}" alt="">Quản trị
+                    <i class="fas fa-users-cog"></i>Quản trị
                 </a>
                 @endif
                 <a class="dropdown-item dropdown-item-custom" href="{{ route('user.infomation', Auth::user()->id) }}">
-                    <img src="{{asset('images/user-icon.svg')}}" alt="">Hồ sơ cá nhân
+                    <i class="fas fa-user"></i>Hồ sơ cá nhân
                 </a>
                 <a class="dropdown-item dropdown-item-custom" href="{{ route('user.history', Auth::user()->id) }}">
-                    <img src="{{asset('images/history-icon.svg')}}" alt="">Lịch sử mượn sách
+                    <i class="fas fa-history"></i>Lịch sử mượn sách
                 </a>
                 <a class="dropdown-item dropdown-item-custom" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
-                    <img src="{{asset('images/logout-icon.svg')}}" alt="">{{ __('Đăng xuất') }}
+                    <i class="fas fa-sign-out-alt"></i>{{ __('Đăng xuất') }}
                 </a>
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                     @csrf
                 </form>
             </div>
-        @endif
-
+            @endif
+        </div>
         @endguest
     </div>
 </header>
