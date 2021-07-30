1223927534c04bcf6f7bfe2b23a8e1a6953af6a1
khuong1542 <79191404+khuong1542@users.noreply.github.com>
2021-07-30 17:58:35 +0700

    update-main

--- resources/views/client/pages/book-detail.blade.php
+++ resources/views/client/pages/book-detail.blade.php
@@ -16,13 +16,8 @@
     @endif
 
 
-<<<<<<< HEAD
-<main>
-    <div class="book-detail-content row">
-=======
     <div class="container">
         <div class="book-detail-content row">
->>>>>>> main
 
             <div class="col-md-4 book-cover">
                 <div class="book-cover__wrapper">
@@ -45,34 +40,23 @@
                         </div>
                         <div class="book-info-rating">
                             <div class="rate-stars">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                @for ($i = 0; $i < floor($avg_rating); $i++) <i
-                                        class="fas fa-star"></i>
+                                @for ($i=1; $i <= 5; $i++) 
+                                    @if (round($avg_rating,1) >= round($i,1) )
+                                        <i class="fas fa-star"></i>
+                                    @else
+                                        <i class="far fa-star"></i>
+                                    @endif
+                                    <!-- <i class="fas fa-star"></i> -->
                                 @endfor
+                                @if($avg_rating>0)
+                                    {{ round($avg_rating,1) }}
+                                @endif
                             </div>
-                            <span class="review-count ">( 352 )</span>
+                            <span class="review-count ">( {{ count($rates) }} đánh giá )</span>
                         </div>
 
                     </div>
                     <div class="book-button-group">
-
-<<<<<<< HEAD
-                    @if ($ordered)
-                    <div class="book-button-item">
-                        <a href="{{ route('book.read', $book->id) }}"
-                            class="button button__outline-lg button-custom">Đọc sách</a>
-                    </div>
-                    @else
-                    <div class="book-button-item">
-                        <a href="{{ route('Book.Order', ['id' => $book->id]) }}"
-                            class="button button__background-lg">Mượn sách</a>
-                    </div>
-                    <div class="book-button-item">
-                        <a href="{{ route('book.read', ['id' => $book->id]) }}" class="button button__outline-lg ">Xem
-                            trước</a>
-=======
                         @if ($ordered)
                             <div class="book-button-item">
                                 <a href="{{ route('book.read', $book->id) }}"
@@ -106,15 +90,8 @@
                                     class="button button__outline-sm">{{ $cate->name }}</a>
                             </div>
                         @endforeach
->>>>>>> main
                     </div>
                 </div>
-<<<<<<< HEAD
-                <div class="book-info__description">
-                    <div class="book-description__wrapper">
-                        <div class="book-description__text">
-                            {!! $book->description !!}
-=======
             </div>
         </div>
 
@@ -152,24 +129,9 @@
                                         luận</button>
                                 </div>
                             </form>
->>>>>>> main
                         </div>
                     </div>
                 </div>
-
-<<<<<<< HEAD
-                <div class="book-info__tags">
-                    @foreach ($book->categories as $cate)
-                    <div class="info-tag__item">
-                        <a href="{{route('book.category',$cate->slug)}}"
-                            class="button button__outline-sm">{{ $cate->name }}</a>
-                    </div>
-                    @endforeach
-
-                </div>
-            </div>
-        </div>
-=======
                 <div class="book-tabs__comment tab-pane" id="review-tab">
                     @foreach ($rates as $rate)
                         @if ($rate->user)
@@ -223,24 +185,6 @@
                             <div class="row">
 
                                 @foreach ($sameBooksUnique as $book)
-
-<<<<<<< HEAD
-                <div class="comment-box__wrapper">
-                    <div class="comment-box__image">
-                        <img src="{{Auth::user()->avatar}}" alt="">
-                    </div>
-                    <div class="comment-box__content">
-                        <form action="{{ route('comments.store') }}" method="post">
-                            @csrf
-                            <div class="comment__input">
-                                <textarea class="form-control" name="body" rows="2"
-                                    placeholder="What are you thinking?"></textarea>
-                                <input type=hidden name=book_id value="{{ $book->id }}" />
-                            </div>
-                            <div class="comment__btn">
-                                <button type="submit" class="button button__background-lg button-comment">Bình
-                                    luận</button>
-=======
                                 @if ($loop->index < 4) <div class="col-3">
                                     <div class="book-card ">
                                         <div class="book-card__img">
@@ -272,7 +216,6 @@
                                             <a href="{{ route('Book.Order', $book->id) }}" class="borrow-btn">Mượn sách</a><a href="{{ route('book.read', $book->id) }}" class="review-btn">Xem trước</a>
                                         </div>
                                     </div>
->>>>>>> main
                             </div>
                             @endif
                             @endforeach
@@ -282,30 +225,6 @@
                     @if (count($sameBooksUnique) > 4)
                     <div class="carousel-item">
                         <div class="row">
-
-<<<<<<< HEAD
-            <div class="book-comment__body tab-pane" id="review-tab">
-                @foreach ($rates as $rate)
-                @if ($rate->user)
-                <div class="book-comment-body__detail">
-                    <div class="book-comment-body-detail__img">
-                        <img src="{{ asset($rate->user->avatar) }}" alt="" class="rounded-circle" width="40">
-                    </div>
-                    <div class="book-comment-body-detail__content">
-                        <div class="book-comment-body-detail__username">{{ $rate->user->name }}</div>
-                        <div class="book-comment-body-detail__date">
-                            <span class="book-star">
-                                @for ($i = 0; $i < $rate->rating; $i++)
-                                    <i class="fas fa-star text-"></i>
-                                    @endfor
-                                    <span>
-                        </div>
-                        <div class="book-comment-body-detail__comment">{{ $rate->body }}</div>
-                    </div>
-                </div>
-                @endif
-                @endforeach
-=======
                             @foreach ($sameBooksUnique as $book)
                             @if ($loop->index >= 4 && $loop->index < 8) <div class="col-3">
                                 <div class="book-card ">
@@ -356,7 +275,6 @@
                     
                 </a>
                 @endif
->>>>>>> main
             </div>
 
 
@@ -508,21 +426,6 @@
 
 
     <script>
-<<<<<<< HEAD
-    let readMore = document.querySelector('#js-read-more');
-    let desc = document.querySelector('.book-description__text');
-    console.log(desc.offsetHeight);
-    if (desc.offsetHeight < 240) {
-        desc.parentNode.style.height = "auto";
-        readMore.style.display = "none";
-    }
-
-    readMore.addEventListener('click', () => {
-        let x = readMore.parentElement.querySelector('.book-description__wrapper')
-        x.classList.toggle("show-more");
-        (x.classList.contains('show-more')) ? readMore.innerHTML = "Ẩn bớt ": readMore.innerHTML = "Xem thêm ";
-    })
-=======
         let readMore = document.querySelector('#js-read-more');
         let desc = document.querySelector('.book-description__text');
         console.log(desc.offsetHeight);
@@ -569,7 +472,6 @@
                 // console.log(item.parentElement.parentElement.parentElement.parentElement.parentElement.querySelector('.comment-box__wrapper').classList.remove('comment-box__hidden'))
             })
         })
->>>>>>> main
     </script>
 
 
